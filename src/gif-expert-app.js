3b2af93ad1fbdcbf5a468bf53519b6cdcb8d4f8f
Juan Baltierrez <juanbal2808@gmail.com>
2021-02-04 20:01:14 -0600

    Logo as React Component

--- src/gif-expert-app.js
+++ src/gif-expert-app.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import {AddCategory} from "./components/AddCategory";
 import {GifGrid} from "./components/GifGrid";
 
-import Logo from './img/logo.png';
+import {ReactComponent as Logo } from './img/Logo.svg';
 
 export const GifExpertApp = (props) => {
 
@@ -13,13 +13,12 @@ export const GifExpertApp = (props) => {
         <>
             <header className="w-full flex items-center" style={{height: "50px", padding: "5px 20px"}}>
                 <div className="w-1/5 h-full">
-                    <img className="w-auto h-full" src={Logo} alt={"Gif app"}/>
+                    <Logo className="w-auto h-full"  alt={"Gif app"}/>
                 </div>
                 <a className="flex w-4/5 h-full text-center items-center justify-end"  href="https://www.giphy.com">
                     <h3>Gifs by</h3><img className="h-4/5" src="https://media.giphy.com/media/igz3CtPVyFj975etI5/giphy.gif" alt=""/>
                 </a>
             </header>
-
             <div style={{padding: "30px 60px"}}>
                 <h1 className="text-4xl font-bold">
                     This app use the Giphy API for search gifs.
